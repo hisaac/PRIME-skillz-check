876a437822389e56037428d6dec9fb7e861c05f1
Isaac Halvorson <hello@hisaac.net>
2016-10-31 16:08:07 -0500

    time's up

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 
 
 	// 2 - Change the text of the h1 element to something (anything!) else.
-    
+    $('h1').text('look!');
 
   // 3 - Get rid of (delete) the useless unordered list (ul) by first
   // selecting the h2 nested within its list item.
+
+  // I ran out of time :(
 
 });
