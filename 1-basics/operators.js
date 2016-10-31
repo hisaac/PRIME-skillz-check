2f245a67d49003e3552a097e1f42a5cbc3e175d8
Isaac Halvorson <hello@hisaac.net>
2016-10-31 14:43:32 -0500

    complete section 1 and create checker.html file to test

--- 1-basics/operators.js
+++ 1-basics/operators.js
@@ -1,7 +1,15 @@
 // 1 - Assign the value 22 to a variable called sigma
+var sigma = 22;
 
 // 2 - Create a new variable called peeps and assign it the value of sigma
+var peeps = sigma;
 
 // 3 - Decrease the value of peeps by 1
+peeps--;
 
 // 4 - Write a console log that checks if sigma is equal to peeps
+if (sigma === peeps){
+    console.log("sigma and peeps are equal");
+} else {
+    console.log("sigma and peeps are not equal");
+}
