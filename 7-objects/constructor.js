bc46f03626ad3910ccc2c2c2f1837d7ae2af9a92
Isaac Halvorson <hello@hisaac.net>
2016-10-31 15:58:32 -0500

    complete section 7

--- 7-objects/constructor.js
+++ 7-objects/constructor.js
@@ -6,7 +6,22 @@
 // The object should have two functions
 // - area() returns the area of this triangle
 // - perimeter() returns the perimeter of this triangle
+function RightTriangle (side1, side2, base) {
+    this.area = function () {
+        return side1 * base / 2;
+    }
+    this.perimeter = function () {
+        return side1 + side2 + base;
+    }
+}
 
 // 2 - Create two instances of the RightTriangle
 // object. Console log the results of calling
 // both functions on both instances.
+var triangle1 = new RightTriangle (11, 12, 13);
+var triangle2 = new RightTriangle (14, 15, 16);
+
+console.log("triangle1's area:", triangle1.area());
+console.log("triangle1's perimeter:", triangle1.perimeter());
+console.log("triangle2's area:", triangle2.area());
+console.log("triangle2's perimeter:", triangle2.perimeter());
--- 7-objects/literal.js
+++ 7-objects/literal.js
@@ -5,15 +5,28 @@
 // - zip
 // Assign the values based on Prime's
 // address.
+var primeLocation = {
+    city: "Bloomington",
+    state: "MN",
+    zip: 55431
+}
 
 // 2 - Using dot notation, create a
 // new property for prime. This property
-// will be called address and will 
+// will be called address and will
 // take the value of Prime's street
 // address.
+primeLocation.address = "9401 James Ave S";
 
 // 3 - Using dot notation, create a
 // new method for prime. This method
 // will console log out the complete
 // address of prime. Call the new
 // property/method print.
+primeLocation.print = function(){
+    console.log(
+        "Prime Academy's address is:\n" +
+        this.address + "\n" +
+        this.city + ", " + this.state + " " + this.zip
+    );
+}
