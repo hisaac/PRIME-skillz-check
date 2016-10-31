da8ac9189f30c146c5047c7cf0d801fe0cd0a01c
Isaac Halvorson <hello@hisaac.net>
2016-10-31 15:14:08 -0500

    complete section 4

--- 4-arrays/advanced.js
+++ 4-arrays/advanced.js
@@ -1,16 +1,21 @@
 // 1 - Use a built-in method of Arrays
 // to add the value 32 to the end of
 // the following array.
-
 var arr = [3, 4, 6, 0];
+arr.push(32);
 
 // 2 - Remove the values 4 & 6 from
 // arr using the splice method.
+arr.splice(1, 2);
 
 // 3 - Use the sort method to order
 // each array value from smallest
 // to largest.
+arr.sort();
 
 // 4 - Use the forEach method to
 // halve the value of each
 // number in the array.
+arr.forEach(function (entry, index) {
+    arr[index] = entry /2;
+});
