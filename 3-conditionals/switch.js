0b35aca7ec079cda4896e4c81e17f7c79b5232ed
Isaac Halvorson <hello@hisaac.net>
2016-10-31 14:57:24 -0500

    complete section 3

--- 3-conditionals/switch.js
+++ 3-conditionals/switch.js
@@ -1,4 +1,32 @@
-// Write a switch statement that 
+// Write a switch statement that
 // console logs witty statements for each
 // day of the standard work week: Monday, Tuesday,
 // Wednesday, Thursday, Friday
+
+var day = '';
+
+switch (day) {
+    case 'Monday':
+    console.log('Here we go again…');
+    break;
+
+    case 'Tuesday':
+    console.log('Well, at least it\'s not Monday.');
+    break;
+
+    case 'Wednesday':
+    console.log('They tell me the week is almost through, but I know \'tis ' +
+      'but a lie.');
+    break;
+
+    case 'Thursday':
+    console.log('Thursdays are the new Fridays, meaning I\'ve already checked out.');
+    break;
+
+    case 'Friday':
+    console.log('If I still worked in advertising, we\'d be drinking.');
+    break;
+
+    default:
+    console.log('Why are you at work?');
+}
